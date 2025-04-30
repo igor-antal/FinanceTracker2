a9aab6e08beeae4aefa48823c4ff5025deff5759
Igor Antal <149603126+Churak-iii@users.noreply.github.com>
2025-04-30 14:57:35 +0200

    basic sql functions, main window and popup window (add entry) and table

---
+++ main.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkcalendar import Calendar
+from sql_functions import fetch_all_data, delete_row, insert_row
+
+def plot_entries():
+    pass
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls):
+        if cls not in cls._instances or not tk.Toplevel.winfo_exists(cls._instances[cls]):
+            cls._instances[cls] = super().__call__()
+
+
+class NewEntryWindow(tk.Toplevel, metaclass=Singleton):
+
+    def __init__(self):
+        super().__init__()
+
+        self.today = datetime.today().timetuple()[:3]
+        self.geometry("600x600")
+        self.calendar = Calendar(self, selectmode="day",
+                                 year=self.today[0], month=self.today[1], day=self.today[2], date_pattern="dd-%m-%Y")
+        self.calendar.pack()
+        self.calendar.grid(column=1)
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Finance Manager")
+        self.geometry("800x600")
+        self.config(background="#fff6f2")
+
+        Button(self, text="Delete Entry", command=self.delete_entry).grid(row=0, column=1)
+        Button(self, text="New Entry", command=self.new_entry_window).grid(row=0, column=2)
+        Button(self, text="Plot Entries", command=plot_entries).grid(row=0, column=3)
+
+        self.table = ttk.Treeview(self, columns=("id", "date", "amount", "type", "description"), show="headings")
+        self.table.column("id", width=0, stretch=tk.NO)
+        self.table.heading("id", text="")
+        self.table.heading("date", text="Date")
+        self.table.heading("amount", text="Amount")
+        self.table.heading("type", text="Type")
+        self.table.heading("description", text="Description")
+        self.table.grid(row=1, column=1, columnspan=3)
+
+    def load_table(self):
+        for item in self.table.get_children():
+            self.table.delete(item)
+        for row in fetch_all_data():
+            self.table.insert('', tk.END, values=row)
+
+    def delete_entry(self):
+        selected_entry = self.table.selection()
+        if not selected_entry:
+            messagebox.showinfo("Info", "Please select an entry for deletion")
+            return
+        confirm = messagebox.askyesno("Confirm Delete", "Are you sure you want to delete the selected entry?")
+        if confirm:
+            row_id = self.table.item(selected_entry[0], "values")
+            delete_row(row_id[0])
+            self.load_table()
+
+    @staticmethod
+    def new_entry_window():
+        return NewEntryWindow()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.load_table()
+    app.mainloop()
+
+
---
+++ sql_functions.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+
+def initiate_sql():
+    conn = sqlite3.connect('finance_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS finance_entries (
+    id INTEGER PRIMARY KEY,
+    date TEXT,
+    amount REAL,
+    type TEXT,
+    description TEXT
+    );''')
+    conn.commit()
+    conn.close()
+
+
+def fetch_all_data():
+    conn = sqlite3.connect('finance_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    SELECT * FROM finance_entries;
+    ''')
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def insert_row(date, amount, type, description):
+    conn = sqlite3.connect('finance_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO finance_entries (date, amount, type, description) VALUES (?, ?, ?, ?)'''
+                   , (date, amount, type, description))
+    conn.commit()
+    conn.close()
+
+
+def delete_row(row_id):
+    conn = sqlite3.connect('finance_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    DELETE FROM finance_entries WHERE id = ?
+    ''', (row_id))
+    conn.commit()
+    conn.close()
